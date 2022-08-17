73c59c65d54023d19d69c42ea545bc1ae5174343
KarpovaO <grose@yandex.ru>
2022-08-17 21:12:47 +0300

    POST

--- api-ci/rest/src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ api-ci/rest/src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,21 +1,29 @@
 package ru.netology.rest;
 
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 class MobileBankApiTestV1 {
     @Test
-    void shouldReturnDemoAccounts() {
+    void shouldCheckBodyAndHead() {
         // Given - When - Then
-        // Предусловия
+// Предусловия
         given()
-                .baseUri("http://localhost:9999/api/v1")
-                // Выполняемые действия
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("something, no matter what")
                 .when()
-                .get("/demo/accounts")
-                // Проверки
+                .post("/post")
                 .then()
-                .statusCode(200);
+                .statusCode(200)
+                .body("data", equalTo("something, no matter what"))
+                .body("url", equalTo("https://postman-echo.com/post"))
+                .header("Content-Type", "application/json; charset=utf-8")
+                .header("Vary", "Accept-Encoding")
+        ;
     }
 }
